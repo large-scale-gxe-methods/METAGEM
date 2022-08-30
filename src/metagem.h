c37010ddfce7fb1ef696275f1188f143f7918beb
Duy T. Pham <39827376+duytpm16@users.noreply.github.com>
2022-08-30 18:56:01 -0500

    Remove Boost where possible

--- src/metagem.h
+++ src/metagem.h
@@ -7,19 +7,22 @@
 #include <unordered_map>
 
 #include <boost/math/distributions/chi_squared.hpp>
-#include <boost/filesystem.hpp>
 
+#include "../include/CLI11-2.2.0/CLI11.hpp"
+#include "../include/sparsepp-2018.2/spp.h"
 #include "matrix_utils.h"
 #include "read_parameters.h"
 #include "file.h"
 
+#define VERSION "1.0"
+
 using std::cout;
 using std::endl;
 using std::cerr;
+using spp::sparse_hash_map;
 
 int flipAllele(std::string a1, std::string a2, std::string b1, std::string b2);
 void metagem(CommandLine cmd);
-
 
 
 #endif
