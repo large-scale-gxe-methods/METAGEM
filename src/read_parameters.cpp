f64313ed6dbc1b25db6836fa899d73ae4ff8496d
sguo429 <148508089+sguo429@users.noreply.github.com>
2024-02-05 10:46:59 -0600

    Update read_parameters.cpp

--- src/read_parameters.cpp
+++ src/read_parameters.cpp
@@ -226,7 +226,7 @@ void print_help() {
         << "   --exposure-names \t The names of the exposure(s) to be included in the meta-analysis." << endl
         << "   --out \t\t Full path and extension to where METAGEM output results. \n \t\t\t    Default: metagem.out" << endl
         << "   --meta-option \t Integer value indicating which summary statistics should be used for meta-analysis. \n\t\t\t    0: Both model-based and robust summary statistics. \n \t\t\t    1: model-based summary statistics. \n \t\t\t    2: robust summary statistics. \n \t\t\t    Default: 0" << endl
-        << "   --additional-test \t Specify one additional test to run." << endl;
+        << "   --additional-test \t Specify the variable names (may or may not include main genetic effect G) and the output file name for one additional test to run." << endl;
     cout << endl << endl;
     cout << endl << endl;
 }
