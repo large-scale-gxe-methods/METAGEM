b0a5eb756565345147de485f308594dd406037e2
sguo429 <148508089+sguo429@users.noreply.github.com>
2024-06-18 22:18:37 +0800

    Update read_parameters.h

--- src/read_parameters.h
+++ src/read_parameters.h
@@ -13,20 +13,26 @@ public:
     std::vector<std::string> lcIntNames;
     std::vector<std::string> intNames2;
     std::vector<std::string> lcIntNames2;
-    std::vector<std::string> additionalTestInfo;
+    std::vector<std::string> intNames3;
+    std::vector<std::string> lcIntNames3;
+    std::vector<std::string> additionalJointInfo;
+    std::vector<std::string> additionalInteractionInfo;
 
     std::string outFile;
     std::string outFile2;
+    std::string outFile3;
     std::string metaFileList;
 
     size_t nInt;
     size_t nInt2 = 1;
+    size_t nInt2 = 2;
     size_t nCov;
     int metaOpt;
 
     bool mb = false;
     bool rb = false;
-    bool additionalTest = false;
+    bool additionalJoint = false;
+    bool additionalInteraction = false;
     void processCommandLine(int argc, char* argv[]);
 };
 
