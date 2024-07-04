b42f98a41f5b62abdc15d34c1d39c87f2a396a18
sguo429 <148508089+sguo429@users.noreply.github.com>
2024-07-04 23:56:09 +0800

    Update file.h

--- src/file.h
+++ src/file.h
@@ -40,5 +40,6 @@ class FileInfo {
 void processFileHeader(int nInt1, int nInt2, int nint3, bool mb, bool rb, bool additionalJoint, bool additionalInteraction, std::vector<std::string> lc_intNames, std::vector<std::string> lc_intNames2, std::vector<std::string> lc_intNames3, std::vector<std::string> fileNames, FileInfo* fip);
 void printOutputHeader(bool mb, bool rb, bool additionalJoint, bool additionalInteraction, std::string output, std::string output2, std::string output3, size_t nInt1, size_t nInt2, size_t nInt3, std::vector<std::string> intNames, std::vector<std::string> intNames2, std::vector<std::string> intNames3); 
 void printHeaderMissingError(std::string fileName, std::string column);
+void loadHeaderRenaming(std::string fileHeaderPath, std::vector<std::string> fileNames);
 
 #endif
